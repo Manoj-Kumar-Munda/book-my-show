726d59a41b3169f1750f9e9cc41c0e12f60db0b2
Manoj <manojkrmunda828@gmail.com>
2026-08-23 11:32:36 +0530

    fix: prisma and nextjs issue

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,7 @@ export default async function Home() {
 
   return (
     <div className="text-black">
+      <h1>Book my show</h1>
       <div>{users?.username}</div>
       <div>{users?.password}</div>
     </div>
